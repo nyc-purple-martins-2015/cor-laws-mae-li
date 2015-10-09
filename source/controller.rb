51bdbb6b33d73e6d9abcc10429ab5dd43c72dd28
Corey & Mae Capozzi <pair+cgrad01.maecapozzi@devbootcamp.com>
2015-10-09 15:00:44 -0400

    add initial work

---
+++ source/controller.rb
@@ -0,0 +1,10 @@
+require_relative 'game'
+require_relative 'card'
+
+game = Game.new()
+game.load_cards('flashcard_samples.txt')
+game.create_cards
+
+
+
+
---
+++ source/game.rb
@@ -0,0 +1,39 @@
+class Game
+
+  attr_reader :deck, :load_array, :questions, :answers
+
+  def initialize(args = {})
+    @deck = []
+    @load_array =[]
+    @questions = []
+    @answers = []
+  end
+
+  def load_cards(filename)
+    File.open(filename).each do |line|
+      @load_array << line.chomp
+      @load_array.keep_if {|item| item != ""}
+    end
+  end
+
+  def create_cards
+    @load_array.each_with_index do |line, index|
+      if index%2 == 0
+        @questions << line
+      elsif index%2 != 0
+        @answers << line
+      end
+    end
+    @questions.each_with_index { |question, index| @deck << Card.new(question: question, answer: @answers[index]) }
+  end
+
+  def start_game
+    "Welcome to Flashcards. Would you like to play?"
+  end
+
+end
+
+
+
+
+# start game, ask questions, receive answers, give results, and move to next question

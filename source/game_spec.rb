08362cbdc15da7ba013003644ae3feccd93d9ca9
Lawson Marlowe <lawsonmarlowe@gmail.com>
2015-10-11 15:40:04 -0400

    Create rspec for first two methods of game.rb and add my questions.txt

---
+++ source/game_spec.rb
@@ -0,0 +1,51 @@
+require_relative 'game'
+require_relative 'card'
+
+describe 'Game' do
+let(:card) {Card.new(question: "What is coding?", answer: "fun")}
+let(:game) {Game.new}
+
+ describe '#load_cards' do
+   it 'should load an array of strings from a file' do
+    game.load_cards('flashcard_samples2.txt')
+    expect(game.load_array[0]).to be_a(String)
+   end
+   it 'should create an array with odd numbered items as answers' do
+    game.load_cards('flashcard_samples2.txt')
+    expect(game.load_array[3]).to eql("and")
+   end
+   it 'should create an array with even numbered items as questions' do
+    game.load_cards('flashcard_samples2.txt')
+    expect(game.load_array[2]).to eql("A command that appends two or more objects together.")
+   end
+ end
+
+  describe '#create_cards' do
+    it 'each item in @questions should be a question' do
+      game.load_cards('flashcard_samples2.txt')
+      game.create_cards
+      expect(game.questions[0].split(" ").length).to be > 3
+    end
+    it 'each item in @answers should be an answer' do
+      game.load_cards('flashcard_samples2.txt')
+      game.create_cards
+      expect(game.answers[0].split(" ").length).to be < 3
+    end
+    it 'should return an array of objects called @deck with an attribute called \'question\'' do
+      game.load_cards('flashcard_samples2.txt')
+      game.create_cards
+      expect(game.deck[0].question).to eql("To create a second name for the variable or method.")
+    end
+    it 'should return an array of objects called @deck with an attribute called \'answer\'' do
+      game.load_cards('flashcard_samples2.txt')
+      game.create_cards
+      expect(game.deck[0].answer).to eql("alias")
+    end
+    it 'should return an array of objects called @deck with an attribute called \'status\'' do
+      game.load_cards('flashcard_samples2.txt')
+      game.create_cards
+      expect(game.deck[0].status).to eql(false)
+    end
+  end
+
+end
--- source/runner.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'game'
-require_relative 'card'
-require_relative 'view'
-require_relative 'controller'
-
-  if ARGV[0] == "begin"
-    ruby controller.rb
-  end
